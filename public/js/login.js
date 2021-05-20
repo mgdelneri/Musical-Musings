cd9931661f498b1e6b18de5a5ad8a5752b41d9a2
Marina <mgdelneri@gmail.com>
2021-05-19 20:14:27 -0500

    Continued styling form boxes

--- public/js/login.js
+++ public/js/login.js
@@ -14,7 +14,7 @@ const loginFormHandler = async function(event) {
     });
   
     if (response.ok) {
-      document.location.replace('/login');
+      document.location.replace('/dashboard');
     } else {
       alert('Failed to login!');
     }
