30d123573b4a94dff43eda92499959ab3644c038
dustingottlieb <dustingottlieb@gmail.com>
2021-05-22 11:48:29 -0500

    added cloudinary API

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 const Sequelize = require('sequelize');
+const cloudinary = require('cloudinary');
 
 require('dotenv').config();
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
+cloudinary.config({
+    cloud_name: 'dustingottlieb',
+    api_key: '384262337385414',
+    api_secret: 'I892sxli4ZxOTCvgQPdGIN_hZ9k'
+});
+
+const sequelize = process.env.JAWSDB_URL ?
+    new Sequelize(process.env.JAWSDB_URL) :
+    new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
     });
 
 module.exports = sequelize;
