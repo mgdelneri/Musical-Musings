1679303175ba45cae472f0f93bd917c3d63b6a7b
Alex Norton <atn362@gmail.com>
2021-05-22 10:27:27 -0500

    added exchange api and others

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
+const exchangeRoutes = require('./exchange-routes');
 
 router.use('/user', userRoutes);
 router.use('/post', postRoutes);
 router.use('/comment', commentRoutes);
+router.use('/exchange', exchangeRoutes);
 
 module.exports = router;
